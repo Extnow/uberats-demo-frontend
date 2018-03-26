1fe30f6bc997bf333a8a7e9ac01bd2568cce49c9
Ryabushkin Mikhail <ryabushkin.mikhail@gmail.com>
2018-03-26 16:41:14 +0300

    добавил в проект плагины для галпа

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,30 @@
 const gulp = require("gulp"),
   sass = require("gulp-sass"),
   imagemin = require("gulp-imagemin"),
-  autoprefixer = require("gulp-autoprefixer");
+  autoprefixer = require("gulp-autoprefixer"),
+  minifyCss = require("gulp-clean-css"),
+  clean = require("gulp-clean"),
+  plumber = require("gulp-plumber"),
+  changed = require("gulp-changed"),
+  browserSync = require("browser-sync").create(),
+  run = require("run-sequence");
 
 gulp.task("scss", function () {
   return gulp.src("src/scss/main.scss")
-    .pipe(sass({ includePaths: ["src/scss"] }).on("error", sass.logError))
+    .pipe(changed("build/css"))
+    .pipe(plumber())
+    .pipe(sass().on("error", sass.logError))
     .pipe(autoprefixer({
       browsers: ["last 2 versions"]
     }))
-    .pipe(gulp.dest("build/css"));
+    .pipe(minifyCss())
+    .pipe(gulp.dest("build/css"))
+    .pipe(browserSync.stream());
 });
 
 gulp.task("img", function () {
   return gulp.src("src/img/**/*")
+    .pipe(changed("build/img"))
     .pipe(imagemin([
       imagemin.optipng({ optimizationLevel: 5 }),
       imagemin.jpegtran({ progressive: true })
@@ -21,8 +32,26 @@ gulp.task("img", function () {
     .pipe(gulp.dest("build/img/"));
 });
 
-gulp.task("scss:watch", function () {
-  gulp.watch("src/scss/**/*.scss", ["scss"]);
+gulp.task("clean", function () {
+  return gulp.src("build", { read: false })
+    .pipe(clean());
 });
 
-gulp.task("default", ["scss", "img", "scss:watch"]);
+gulp.task("build", function (fn) {
+  run("clean", "scss", "img", fn);
+});
+
+gulp.task("serve", function () {
+  browserSync.init({
+    server: ""
+  });
+
+  gulp.watch("src/scss/**/*.scss", ["scss"]);
+  gulp.watch("src/img/**/*", ["img"]);
+  browserSync.watch("*.html").on("change", browserSync.reload);
+});
+
+gulp.task("default", function (fn) {
+  gulp.start("build", "serve", fn);
+});
+
